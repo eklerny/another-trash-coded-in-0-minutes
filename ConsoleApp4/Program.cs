f9b5f09b434204c1f715c4fca3c71494b35e9a7b
ekler <ekler@EKLERNY>
2022-09-14 11:13:51 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string lang = "de";
+
+            switch (lang)
+            {
+                case "ru":
+                    Console.WriteLine("Привет Мир!");
+                    break;
+                case "en":
+                    Console.WriteLine("Hello World!");
+                    break;
+                case "de":
+                    Console.WriteLine("Hallo, Welt!");
+                    break;
+                case "jp":
+                    Console.WriteLine("こんにちは、世界!");
+                    break;
+                case "kz":
+                    Console.WriteLine("Сәлем Әлем!");
+                    break;
+
+                default:
+                    Console.WriteLine("Локализация не найдена!");
+                    break;
+            }
+        }
+    }
+}
